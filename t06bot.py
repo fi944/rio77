4cdfae8447430e8cdab6e140750a03e256cfb96d
fi944 <132758667+fi944@users.noreply.github.com>
2023-05-10 10:00:21 +0300

    Update t06bot.py

--- t06bot.py
+++ t06bot.py
@@ -15,42 +15,42 @@ jepthon = ['yes']
 async def _(event):
     if jepthon[0] == "yes":
         await event.edit("**᯽︙سيتم تجميع النقاط , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
-        channel_entity = await jepiq.get_entity(bot_username)
+        channel_entity = await sedthon.get_entity(bot_username)
         await jepiq.send_message('@zmmbot', '/start')
         await asyncio.sleep(5)
-        msg0 = await jepiq.get_messages('@zmmbot', limit=1)
+        msg0 = await sedthon.get_messages('@zmmbot', limit=1)
         await msg0[0].click(2)
         await asyncio.sleep(5)
-        msg1 = await jepiq.get_messages('@zmmbot', limit=1)
+        msg1 = await sedthon.get_messages('@zmmbot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
         for i in range(100):
-            if jepthon[0] == 'no':
+            if ispay[0] == 'no':
                 break
             await asyncio.sleep(5)
 
-            list = await jepiq(GetHistoryRequest(peer=channel_entity, limit=1,
+            list = await sedthon(GetHistoryRequest(peer=channel_entity, limit=1,
                                                    offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
             msgs = list.messages[0]
             if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
-                await jepiq.send_message(event.chat_id, f"**لاتوجد قنوات للبوت**")
+                await sedthon.send_message(event.chat_id, f"**لاتوجد قنوات للبوت**")
                 break
             url = msgs.reply_markup.rows[0].buttons[0].url
             try:
                 try:
-                    await jepiq(JoinChannelRequest(url))
+                    await sedthon(JoinChannelRequest(url))
                 except:
                     bott = url.split('/')[-1]
-                    await jepiq(ImportChatInviteRequest(bott))
-                msg2 = await jepiq.get_messages('@zmmbot', limit=1)
+                    await sedthon(ImportChatInviteRequest(bott))
+                msg2 = await sedthon.get_messages('@zmmbot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
-                await jepiq.send_message("me", f"تم الاشتراك في {chs} قناة")
+                await sedthon.send_message("me", f"تم الاشتراك في {chs} قناة")
             except:
-                await jepiq.send_message(event.chat_id, f"**خطأ , ممكن تبندت**")
+                await sedthon.send_message(event.chat_id, f"**خطأ , ممكن تبندت**")
                 break
-        await jepiq.send_message(event.chat_id, "**تم الانتهاء من التجميع !**")
+        await sedthon.send_message(event.chat_id, "**تم الانتهاء من التجميع !**")
 
     else:
         await event.edit("يجب الدفع لاستعمال هذا الامر !")
